172e5d558a2a3a2e0c853f623b89e21ba551455b
Yuri Dev <180821520+mscbuild@users.noreply.github.com>
2026-04-17 21:30:36 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,19 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SelectField, TextAreaField, FileField
+from wtforms import StringField, PasswordField, TextAreaField, IntegerField, SelectField, FileField
 from wtforms.validators import DataRequired
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    role = SelectField('Role', choices=[('worker','Worker'), ('employer','Employer')])
+    username = StringField(validators=[DataRequired()])
+    password = PasswordField(validators=[DataRequired()])
+    role = SelectField(choices=[('worker','Worker'),('employer','Employer')])
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField(validators=[DataRequired()])
+    password = PasswordField(validators=[DataRequired()])
 
 class JobForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    description = TextAreaField('Description', validators=[DataRequired()])
+    title = StringField(validators=[DataRequired()])
+    description = TextAreaField(validators=[DataRequired()])
+    salary = IntegerField()
+    category = StringField()
+    city = StringField()
 
 class ResumeForm(FlaskForm):
-    resume = FileField('Resume')
+    resume = FileField()
