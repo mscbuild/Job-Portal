c2b6aaed39c05e4b4e02b1377d1bd1c32202dbc1
Yuri Dev <180821520+mscbuild@users.noreply.github.com>
2026-04-17 22:03:47 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = "resumes"
-    MAX_CONTENT_LENGTH = 2 * 1024 * 1024
